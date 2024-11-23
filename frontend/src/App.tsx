68b0dc98baeefaeb89ad475e370c98af3ec2f1d1
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2024-11-23 10:52:25 -0500

    setup for firebase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,3 @@
-// import './App.css'
-
-// import { Routes, Route } from 'react-router-dom';
-// import Frontpage from './pages/frontpage';
-// import Detailpage from './pages/detailPage';
-// import Profile from './pages/profile';
-// import Mainpage from './pages/mainpage';
-
-// const init: React.FC = () => {
-//   return(
-//     <div>
-//       <Routes>
-//         <Route path="/frontpage" element={<Frontpage />} />
-//         <Route path="/detailpage" element={<Detailpage />} />
-//         <Route path="/mainpage" element={<Mainpage />} />
-//         <Route path="/profile" element={<Profile />} />
-
-//       </Routes>
-//     </div>
-//   );
-// };
-
-// function App() {
-//   return (
-//     <div>
-//       <Routes>
-        
-//       </Routes>
-//     </div>
-//   );
-// }
-
-// export default App
-
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import Frontpage from './pages/frontpage';
@@ -71,15 +37,4 @@ const Init: React.FC = () => {
   );
 };
 
-// function App() {
-//   return (
-//     <div>
-//       <Routes>
-//         {/* This could eventually be extended for layouts or redirects */}
-//       </Routes>
-//     </div>
-//   );
-// }
-
 export default Init;
-
