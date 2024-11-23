4da85cdbdbf053d9125413e584171c5fc137351d
Minsu <minus9521@gmail.com>
2024-11-22 23:16:16 -0500

    Connect firebase

--- backend/housing/views.py
+++ backend/housing/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
+cred = credentials.Certificate('')
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "",
+})
 
 def index(request):
-    return HttpResponse("Hello from the housing app.")
+    name = db.reference('Data/Name').get()
+    return HttpResponse(name)
