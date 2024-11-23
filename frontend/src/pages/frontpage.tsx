154bbf8a933adebc22a9a3116ea9136924656725
Jewoo Lee <jwcairo0502@gmail.com>
2024-11-23 15:14:55 -0500

    frontpage update

--- frontend/src/pages/frontpage.tsx
+++ frontend/src/pages/frontpage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Search, LogIn } from 'lucide-react'
 import styles from '../components/ApartmentFinder.module.css'
 import { get, ref } from 'firebase/database';
@@ -6,15 +6,15 @@ import { db } from '../firebase';
 import Login from '../pages/login';
 import Signup from '../pages/signup';
 
-const frontpage: React.FC = () => {
-  return (
-    <div>
-      <h1>Welcome to McGill Apartment Finder</h1>
+function frontpage() {
 
-      <Login />
-      <Signup />
+  return (
+    <div className = "frontpage">
+      <button>Log In</button>
+      <button>Sign Up</button>
     </div>
-  )
+  );
 }
+
 
 export default frontpage;
