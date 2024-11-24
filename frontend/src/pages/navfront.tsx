6270e30526d80cfb8c04d7cdca0f695e3a1d97cd
Jewoo Lee <jwcairo0502@gmail.com>
2024-11-24 00:47:54 -0500

    bug fix on front page

--- frontend/src/pages/navfront.tsx
+++ frontend/src/pages/navfront.tsx
@@ -7,13 +7,11 @@ const navfront: React.FC = () => {
    return (
        <header className={styles.header}>
          <div className={styles.headerContent}>
-           <Link to="/" className={styles.logoLink}>
              <img
                src="/images/McGill.png"
                alt="McGill Logo"
                className={styles.logo}
              />
-           </Link>
            <h1 className={styles.appName}>Martlet's Nest</h1>
          </div>
        </header>
